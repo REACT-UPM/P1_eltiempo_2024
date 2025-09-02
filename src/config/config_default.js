e4477161f84512f18891c1fd1783fe60625c52f8
Enrique <enrique.barra@upm.es>
2025-09-02 09:58:50 +0200

    2025 version

--- src/config/config_default.js
+++ src/config/config_default.js
@@ -1,11 +1,11 @@
 const CONFIG = {
+  use_server: true,
   server_url: "<SERVER_URL>",
   api_key: "<YOUR_API_KEY>",
-  num_items_query: 8,
-  num_items_show: 6,
   default_lat: 40.416775,
   default_lon: -3.703790,
-  use_server: true,
+  num_items_query: 8,
+  num_items_show: 6,  
   force_error: false  
 }
 
